f5e1e3e123fbb93d08167eb8f0ed1814e19095b0
Dsblima <dsbl@ecomp.poli.br>
2026-05-04 18:57:03 -0300

    Move tokens to the env variable.

--- src/modules/auth/config/auth.ts
+++ src/modules/auth/config/auth.ts
@@ -1,17 +1,18 @@
 import { SignOptions } from "jsonwebtoken";
 
+import "dotenv/config";
 type TokenConfig = {
   secret_token: string,
-    expires_in_token: SignOptions['expiresIn'],
-    secret_refresh_token: string,
-    expires_in_refresh_token: string,
-    expires_in_refresh_token_days: number,
+  expires_in_token: SignOptions['expiresIn'],
+  secret_refresh_token: string,
+  expires_in_refresh_token: string,
+  expires_in_refresh_token_days: number,
 }
 
 export const authConfig: TokenConfig = {
-    secret_token: '08b9d906f1f2534056d561f74bdd6dd9',
-    expires_in_token: '15m' as const,
-    secret_refresh_token: 'f807f108a5001a2cb9540a23588b4e02',
-    expires_in_refresh_token: '30d',
-    expires_in_refresh_token_days: 30,
+  secret_token: String(process.env.SECRET_TOKEN),
+  expires_in_token: '15m' as const,
+  secret_refresh_token: String(process.env.SECRET_REFRESH_TOKEN),
+  expires_in_refresh_token: '30d',
+  expires_in_refresh_token_days: 30,
 };
